31a6e94580d35d6255a271583e3700a1216b804d
Spayralbe <stefan@thehyve.nl>
2023-06-12 16:44:46 +0200

    Add vocab schema placeholder + hierarchy view

--- other/vocab_view.sql
+++ other/vocab_view.sql
@@ -1,13 +1,15 @@
 -- Set search path to the correct schema if running this script independently
 -- SET search_path TO <dataset_schema>;
 
-CREATE OR REPLACE VIEW concept AS (SELECT * FROM vocab.concept);
-CREATE OR REPLACE VIEW concept_relationship AS (SELECT * FROM vocab.concept_relationship);
-CREATE OR REPLACE VIEW concept_ancestor AS (SELECT * FROM vocab.concept_ancestor);
-CREATE OR REPLACE VIEW concept_synonym AS (SELECT * FROM vocab.concept_synonym);
-CREATE OR REPLACE VIEW concept_class AS (SELECT * FROM vocab.concept_class);
-CREATE OR REPLACE VIEW domain AS (SELECT * FROM vocab.domain);
-CREATE OR REPLACE VIEW vocabulary AS (SELECT * FROM vocab.vocabulary);
-CREATE OR REPLACE VIEW relationship AS (SELECT * FROM vocab.relationship);
-CREATE OR REPLACE VIEW drug_strength AS (SELECT * FROM vocab.drug_strength);
-CREATE OR REPLACE VIEW source_to_concept_map AS (SELECT * FROM vocab.source_to_concept_map);
+CREATE OR REPLACE VIEW concept AS (SELECT * FROM <vocab_schema>.concept);
+CREATE OR REPLACE VIEW concept_relationship AS (SELECT * FROM <vocab_schema>.concept_relationship);
+CREATE OR REPLACE VIEW concept_ancestor AS (SELECT * FROM <vocab_schema>.concept_ancestor);
+CREATE OR REPLACE VIEW concept_synonym AS (SELECT * FROM <vocab_schema>.concept_synonym);
+CREATE OR REPLACE VIEW concept_class AS (SELECT * FROM <vocab_schema>.concept_class);
+CREATE OR REPLACE VIEW domain AS (SELECT * FROM <vocab_schema>.domain);
+CREATE OR REPLACE VIEW vocabulary AS (SELECT * FROM <vocab_schema>.vocabulary);
+CREATE OR REPLACE VIEW relationship AS (SELECT * FROM <vocab_schema>.relationship);
+CREATE OR REPLACE VIEW drug_strength AS (SELECT * FROM <vocab_schema>.drug_strength);
+CREATE OR REPLACE VIEW source_to_concept_map AS (SELECT * FROM <vocab_schema>.source_to_concept_map);
+
+CREATE OR REPLACE VIEW <results_schema>.concept_hierarchy AS (SELECT * FROM <vocab_schema>.concept_hierarchy);
